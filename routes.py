bb3c18942ea3f8822ebad48b6e389993a3d038a2
Alibaba <khaninsq@gmail.com>
2021-05-10 14:02:30 +0300

    Change routes

--- routes.py
+++ routes.py
@@ -2,5 +2,5 @@ from views import search, locations
 
 
 def setup_routes(app):
-    app.router.add_get('/search', search)
-    app.router.add_get('/locations', locations)
+    app.router.add_get('/api/v1/weather/search', search)
+    app.router.add_get('/api/v1/weather/locations', locations)
